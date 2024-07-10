2e166560616e90d6caf1bc13e7408f5f9969865b
KenzaDahhoum <kenza.dahhoum@gmail.com>
2024-07-10 21:58:07 +0100

    decod token

--- hrzen_backend/app.py
+++ hrzen_backend/app.py
@@ -92,9 +92,9 @@ def login():
 
     user = Employee.query.filter_by(email=data['email']).first()
     if user and check_password_hash(user.password, data['password']):
-        token = jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(hours=24)}, app.config['JWT_SECRET_KEY'])
+        token = jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(hours=24)}, app.config['JWT_SECRET_KEY'], algorithm=["HS256"])
         print(f"Token generated: {token}")
-        return jsonify({'token': str(token), 'role': user.role}), 200
+        return jsonify({'token': token, 'role': user.role}), 200
     return jsonify({'message': 'Invalid credentials'}), 401
 
 @app.route('/change-password', methods=['POST'])
